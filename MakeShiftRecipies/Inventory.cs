720b8f51aad1cdbcac4f2570ce70f189f89e688c
dj.jellings <dj.jellings@collegeamerica.edu>
2016-10-03 14:09:17 -0600

    Adding the Classes that I had on the other old Repository.

---
+++ MakeShiftRecipies/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Inventory
+/// </summary>
+namespace MakeShiftRecipies
+{
+    public class Inventory
+    {
+        public Inventory()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+    }
+}
---
+++ MakeShiftRecipies/RecipeDSInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace MakeShiftRecipies
+{
+    public interface RecipeDSInterface
+    {
+        long deleteRecipe(Recipe r);
+        int insertRecipe(Recipe r);
+        int updateRecipe(Recipe r);
+        ArrayList findRecipeByName(Recipe r);
+    }
+}
